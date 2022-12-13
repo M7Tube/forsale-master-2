ea58bc270e939a0f10f45136608d3ba3b89f32af
M7Tube <clashroyale.mahh@gmail.com>
2022-12-13 09:38:33 -0800

    fd

--- storage/framework/views/b797877ca5a73cf80bf726ed0fe76427fcefba87.php
+++
@@ -1,9 +0,0 @@
-<div class="container my-5">
-    <h4 class="mb-2"><b><?php echo e(__('Terms and Conditions')); ?></b></h4>
-    <?php if(app()->getLocale() == 'en'): ?>
-        <p><?php echo e($term['en_terms_conditions'] ?? __('Empty')); ?></p>
-    <?php else: ?>
-        <p><?php echo e($term['ar_terms_conditions'] ?? __('Empty')); ?></p>
-    <?php endif; ?>
-</div>
-<?php /**PATH C:\Users\Abo Samer\Desktop\forsale-master\forsale-master\resources\views/livewire/website/terms.blade.php ENDPATH**/ ?>
